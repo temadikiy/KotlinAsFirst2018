3423640043199943308a965ac99fd3274b78e4eb
temadikiy <43263742+temadikiy@users.noreply.github.com>
2019-01-30 22:57:42 +0300

    1 тривиальная из 8 урока

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -83,7 +83,7 @@ data class Circle(val center: Point, val radius: Double) {
      *
      * Вернуть true, если и только если окружность содержит данную точку НА себе или ВНУТРИ себя
      */
-    fun contains(p: Point): Boolean = TODO()
+    fun contains(p: Point): Boolean = p.distance(center) <= radius
 }
 
 /**
